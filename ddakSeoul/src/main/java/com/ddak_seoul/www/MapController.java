f01275f059638ce3194502ce6fb421aae772b7de
buildcode49 <buildcode49@gmail.com>
2025-03-12 00:24:24 +0900

    [2025-03-12|이서희] 프로젝트 첫 업로드

---
+++ ddakSeoul/src/main/java/com/ddak_seoul/www/MapController.java
@@ -0,0 +1,18 @@
+package com.ddak_seoul.www;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MapController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MapController.class);
+	
+	@RequestMapping(value = "/map", method = RequestMethod.GET)
+	public String viewMap() {
+		return "map";
+	}
+}
---
+++ ddakSeoul/src/main/webapp/WEB-INF/views/map.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>지도화면</title>
+<script type="text/javascript" src="./js/map.js" ></script> 
+</head>
+<body>
+	<h3>지도화면</h3>
+</body>
+</html>
